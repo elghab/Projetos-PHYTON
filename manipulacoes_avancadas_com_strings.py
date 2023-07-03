1b7996757675d8fadf5a8d440c618eacbc0dc6ff
elghab <gabrielc.contato@gmail.com>
2023-07-03 20:38:55 -0300

    conclusao da aula 2

--- manipulacoes_avancadas_com_strings.py
+++ manipulacoes_avancadas_com_strings.py
@@ -16,7 +16,7 @@ nota = 8.5
 s1 = 'Voce tirou %f na disciplina de Algoritmos' % nota
 print(s1)
 
-# o "%f" que aparece no meio da string acima sera substituido por o que for definido do lado de fora da string com um sinal de % seguido da variavel
+# o "%f" que aparece no meio da string acima sera substituido por o que for definido do lado de fora da string (o f significa que é do tipo Float) com um sinal de % seguido da variavel
 # no caso ali seria o nota entao resultaria no console em "Voce tirou 8.500000 na disciplina de Algoritmos"
 
 #--
@@ -28,4 +28,12 @@ print(s1)
 # o "%f" que aparece no meio da string acima fará o mesmo que no exemplo acima, o diferencial é que será delimitado o numero de casas decimais para apenas 2 casas apos a virgula.
 # no caso ali seria o valor de nota entao resultaria no console em "Voce tirou 8.500000 na disciplina de Algoritmos" e agora com essa modificação aparecerá apenas 
 # "Voce tirou 8.50 na disciplina de Algoritmos"
+
+#--
+
+nota = 8.5
+disciplina = 'Algoritmos'
+s1 = 'Voce tirou %.2f na disciplina de %s ' % (nota, disciplina)
+print(s1)
+
 
