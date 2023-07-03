1d55c7a1477e0f700c2e5839619df1f27b5389ca
elghab <gabrielc.contato@gmail.com>
2023-07-03 09:20:16 -0300

    inicio das aulas em python

---
+++ Hello_World.py
@@ -0,0 +1,5 @@
+print ('ola mundo') # Imprime o texto strig "Ola mundo".
+print (2+3) # soma os dois numeros inteiros 2+3=5.
+print ('2'+2) # aconteçe um erro pois nao é possivel somar uma string com um inteiro.
+print ('3+5') # irá escrever no console em string "3+5" e nao fazer a soma pois estando entre aspas significa que e uma string e nao inteiro.
+print ('3'+'3') # ira concatenar as duas strings, printando no console (em string) "33".
